56d5ab2f03ab0d9a1b3edaa4c834cc4851895ba6
Evelyn Utterson <e.utterson@hotmail.co.uk>
2015-11-17 16:39:41 +0000

    Saved functions practice progress at end of class

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,71 @@
+require 'date'
+
+def return_10()
+  10
+end
+
+def add(num_1, num_2)
+  num_1  + num_2
+end
+
+def subtract(num_1, num_2)
+  num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+  num_1 * num_2
+end
+
+def divide(num_1, num_2)
+  num_1 / num_2
+end
+
+def length_of_string(s)
+  s.length
+end
+
+def join_string(string_1, string_2)
+  string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(n)
+  case n
+  when 1
+    "January"
+  when 3
+    "March"
+  when 9
+    "September"
+  end
+end
+
+def number_to_short_month_name(n)
+  number_to_full_month_name(n)[0, 3]
+end
+
+def cube_volume(length)
+  length ** 3
+end
+
+def sphere_volume(r)
+  (4 * Math::PI * r**3) / 3
+end
+
+def cylinder_volume(r, h)
+  Math::PI * r**2 * h
+end
+
+def days_to_event(year, month, day)
+  event_date = Date.new(year, month, day)
+  (event_date - Date.today).to_i
+end
+
+def age_from_birthday(year, month, day)
+  birthday = Date.new(year, month, day)
+  ((Date.today - birthday).to_i) / 365
+end
+
---
+++ ruby_functions_practice_runner.rb
@@ -0,0 +1,95 @@
+#will load in functions from the file
+require_relative "./ruby_functions_practice"
+
+#### Example
+test_result = return_10()
+puts "#{test_result} should = 10"
+
+
+##### Integer Play
+number_1 = 10
+number_2 = 5
+add_result = add( number_1, number_2 )
+puts "#{number_1} + #{number_2} = #{add_result}"
+
+subtract_result = subtract( number_1, number_2 )
+puts "#{number_1} - #{number_2} = #{subtract_result}"
+
+multiply_result = multiply( number_1, number_2 )
+puts "#{number_1} * #{number_2} = #{multiply_result}"
+
+divide_result = divide( number_1, number_2 )
+puts "#{number_1} / #{number_2} = #{divide_result}"
+
+##### String Play
+test_string = "A string of length 21"
+length_of_string = length_of_string(test_string)
+puts "#{test_string} is length #{length_of_string}"
+
+string_1 = "Mary had a little lamb, "
+string_2 = "it's fleece was white as snow"
+joined_string = join_string(string_1, string_2)
+puts "Nursery rhyme: #{joined_string}"
+
+#####Type conversion
+string_1 = '1'
+string_2 = '2'
+add_result = add_string_as_number(string_1, string_2)
+puts "#{string_1} + #{string_2} = #{add_result} Should be 3, (not 12!)"
+
+#####Conditional logic
+first_month_string = number_to_full_month_name(1)
+third_month_string = number_to_full_month_name(3)
+ninth_month_string = number_to_full_month_name(9)
+puts "The first month is #{first_month_string}"
+puts "The third month is #{third_month_string}"
+puts "The ninth month is #{ninth_month_string}"
+
+first_month_string = number_to_short_month_name(1)
+third_month_string = number_to_short_month_name(3)
+ninth_month_string = number_to_short_month_name(9)
+puts "The first month in 3 letters is #{first_month_string}"
+puts "The third month in 3 letters is #{third_month_string}"
+puts "The ninth month in 3 letters is #{ninth_month_string}"
+
+
+##### Further Challenges (not required),  try to write the expectation first if you can.
+
+####http://ruby-doc.org/core-2.1.4/Float.html
+####Given the length of a side of a cube calculate the volume 
+cube_1 = cube_volume(5)
+cube_2 = cube_volume(3)
+puts "The volume of cube 1, which has a length of 5, is #{cube_1}"
+puts "The volume of cube 2, which has a length of 3, is #{cube_2}"
+
+####Given the radius of a sphere calculate the volume
+sphere_1 = sphere_volume(5).round(1)
+puts "I expect sphere_1 to equal 523.6. sphere_1 = #{sphere_1}"
+
+####Can you extend this to calculate other shapes' volumes? (Cylinder, Rectangular Prism, Cone, Capsule)
+cylinder_1 = cylinder_volume(2, 3).round(1)
+puts "I expect cylinder 1, with a radius of 2 and a height of 3, to equal 37.7. It equals #{cylinder_1}"
+
+####http://ruby-doc.org/stdlib-2.1.1/libdoc/date/rdoc/Date.html
+####Days until christmas, Calculate how many nights there are from today until Christmas morning.
+sleeps_to_santa = days_to_event(2015, 12, 25)
+days_to_new_year = days_to_event(2015, 12, 31)
+puts "There are #{sleeps_to_santa} sleeps until Santa's been!"
+puts "It's New Year in #{days_to_new_year} days"
+
+####Given a date of birth, calculate how old a person born on that date would be
+age = age_from_birthday(2003, 8, 12)
+puts "The age of the person is #{age}"
+
+#### Create a command line interface using put, gets to interact with the your functions 
+
+
+
+
+
+
+
+
+
+
+
